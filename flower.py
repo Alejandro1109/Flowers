f324803daeb5062e8ba51e1a895b40b62c78df6e
Alejandro Monzón <alejandroamma8@gmail.com>
2023-12-27 18:03:56 -0500

    First Commit

---
+++ flower.py
@@ -0,0 +1,14 @@
+import turtle 
+
+tortu  = turtle.Turtle()
+
+tortu.color("red")
+tortu.begin_fill()
+tortu.forward(100)
+tortu.left(90)
+tortu.forward(100)
+tortu.left(90)
+tortu.forward(100)
+tortu.left(90)
+tortu.forward(100)
+tortu.end_fill()
